b47d7c3367d44ae958ed0d973e66a3f54f647d92
Evan Druce <drucee@gmail.com>
2016-05-06 17:10:57 -0400

    do sessions pages

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+get '/login' do
+  erb :'login'
+end
+
+post '/login' do
+  @user = User.find_by(name: params[:user][:name])
+
+  if @user && @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect "/users/#{user.id}"
+  else
+    halt(403, "invalid username or password")
+    erb :'login'
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/login'
+end
