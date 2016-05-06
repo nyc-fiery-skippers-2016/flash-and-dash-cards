d7c0691c15548a264ec05693ca2586e4ac118829
David Walden & Diana Ozemebhoya Eromosele & Evan Druce & Megan Swanby & Robert Reith <pair+d-walden.dianaeromosele.drucee.nycswan.robertreith@devbootcamp.com>
2016-05-06 16:24:15 -0400

    Complete associations

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,3 +1,10 @@
 class Guess < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :card
+  belongs_to :round
+  delegate :user, to: :round
+
+  # def user # replaced by the "delegate" in line 5
+  #   self.round.user
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
+
+  has_secure_password
+
+  has_many :guesses, through: :rounds
+  has_many :rounds
 end
