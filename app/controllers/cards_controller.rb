a136a480fb690af521964664bc8839e3081d5220
RobertReith <robertmreith@gmail.com>
2016-05-07 13:22:28 -0400

    Add iteration functionality to cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,11 +5,19 @@ get '/cards' do
 end
 
 post '/rounds/:id/cards' do
+
 	@card = Card.find_by(id: params[:card_id])
+	@next_card = Card.find_by(id: @card.move_to_next_card)
+	@round = Round.find_by(id: params[:id])
 	
 	@guess = Guess.create(response: params[:response], round_id: params[:id], card_id: params[:card_id])
 
-	redirect "/rounds/#{ @guess.round_id }/cards/#{ @card.id }"
+	# check_response = @round.guesses.first.response == @card.answer
+	if @card.deck_id == @next_card.deck_id
+	redirect "/rounds/#{@guess.round_id}/cards/#{@card.move_to_next_card}"
+	else
+		redirect "users/#{current_user.id}"
+	end
 end
 
 get '/rounds/:id/cards/:id' do
