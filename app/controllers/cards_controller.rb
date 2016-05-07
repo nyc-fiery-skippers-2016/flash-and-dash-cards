eaf74b6999ff2abed19338be4b7011de5feb488e
RobertReith <robertmreith@gmail.com>
2016-05-07 11:53:43 -0400

    Add show first card and perist response to db

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,10 +4,20 @@ get '/cards' do
   erb :'cards/index'
 end
 
+post '/rounds/:id/cards' do
+	@card = Card.find_by(id: params[:card_id])
+	
+	@guess = Guess.create(response: params[:response], round_id: params[:id], card_id: params[:card_id])
+
+	redirect "/rounds/#{ @guess.round_id }/cards/#{ @card.id }"
+end
 
 get '/rounds/:id/cards/:id' do
-  @card = Card.find_by(deck_id: params[:deck_id])
+  @card = Card.find_by(id: params[:id])
+  @round = Round.find_by(id: params[:captures][0])
 
   erb :'cards/show'
 end
+
+
 
